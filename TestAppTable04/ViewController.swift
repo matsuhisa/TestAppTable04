c7c6c7a54f64ebcbbe48ccbe7814edf101200a38
Hironobu Matsuhisa <matsuhisa@gmail.com>
2015-03-10 22:22:20 +0900

    Initial Commit

---
+++ TestAppTable04/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAppTable04
+//
+//  Created by 松久浩伸 on 2015/03/10.
+//  Copyright (c) 2015年 matsuhisa.hironobu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
